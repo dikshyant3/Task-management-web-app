50361ce041859101b2670e5bd27c42920c5a5b62
Dikshyant <dhunganadikshyant12@gmail.com>
2022-07-24 22:40:39 +0545

    javascript of notes app

--- notes.js
+++ notes.js
@@ -1,55 +1,41 @@
-console.log("Welcome to notes app. This is notes.js");
-showNotes();
-
-// If user adds a note, add it to the localStorage
-let addBtn = document.getElementById("notes-add");
-addBtn.addEventListener("click", function (e) {
-  let addTxt = document.getElementById("notes_body");
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-  notesObj.push(addTxt.value);
-  localStorage.setItem("notes", JSON.stringify(notesObj));
-  addTxt.value = "";
-  //   console.log(notesObj);
-  showNotes();
+let addBtn = document.getElementById("addBtn");
+const addBox = document.querySelector(".add-box");
+popupBox = document.querySelector(".popup-box");
+popupTitle = popupBox.querySelector("header p");
+tagDesc = popupBox.querySelector("textarea");
+tagTitle = popupBox.querySelector("input");
+closeIcon = popupBox.querySelector("header i");
+const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+addBox.addEventListener("click", () => {
+  popupTitle.innerText = "Add a new Note";
+  addBtn.innerText = "Add Note";
+  popupBox.classList.add("show");
+  // document.querySelector("body").style.overflow = "hidden";
 });
-
-// Function to show elements from localStorage
+closeIcon.addEventListener("click", () => {
+  popupBox.classList.remove("show");
+  // document.querySelector("body").style.overflow="auto"
+});
 function showNotes() {
-  let notes = localStorage.getItem("notes");
-  if (notes == null) {
-    notesObj = [];
-  } else {
-    notesObj = JSON.parse(notes);
-  }
-  let html = "";
-  notesObj.forEach(function (element, index) {
-    html += `
-        <div class="notes_list">
-            <div class="notes_list-item notes_list-item--selected">
-                <div class="notes_small-title">Lecture Notes</div>
-                <div class="notes_small-body">Just Learning Javascript</div>
-                <div class="notes_updated">Tuesday 9:05am</div>
-            </div>
-        </div>`;
+  // if (!notes) return;
+  // document.querySelectorAll(".notes");
+  notes.forEach((notes) => {
+    let liTag = `<li class="notes">
+                    <div class="details">
+                      <p>${notes.title}</p>
+                      <span>${notes.description}</span>
+                    </div>
+                    <div class="bottom-content">
+                      <span>${notes.date}</span>
+                      <div class="settings">
+                        <i class="uil uil-ellipsis-h"></i>
+                        <ul class="menu">
+                          <li><i class="uil uil-pen"></i>Edit</li>
+                          <li><i class="uil uil-trash"></i>Delete</li>
+                        </ul>
+                      </div>
+                    </div>
+                </li>`;
   });
-  let notesElm = document.getElementById("notes");
-  if (notesObj.length != 0) {
-    notesElm.innerHTML = html;
-    //   } else {
-    //     notesElm.innerHTML = `Nothing to show!`;
-    //   }
-  }
 }
-
-/*
-Further Features:
-1. Add Title
-2. Mark a note as Important
-3. Separate notes by user
-4. Sync and host to web server 
-*/
+showNotes();
